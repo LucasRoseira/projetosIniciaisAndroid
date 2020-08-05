22dc6a9a86c560a760ec9a66834b0d2035bbfe40
Lucas Gomes Roseira <lucas@kali>
2020-08-05 01:36:12 -0300

    interface bugada

--- App/Alinhamento2/app/src/main/java/com/example/alinhamento/MainActivity.java
+++ App/Alinhamento2/app/src/main/java/com/example/alinhamento/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.alinhamento;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
